bd13ad3c62385d9859681913fbd5e98f2434c1be
Aniket Bangar <bangaraniket@gmail.com>
2020-12-10 04:12:28 +0530

    Update email_config.py

--- email_config.py
+++ email_config.py
@@ -1,6 +1,6 @@
 
 def Config():
-	gmail_user = 'bangaraniket@gmail.com' # Gmail Username
-	gmail_pwd = 'vkszmdejepdrtcur' #APP Password
+	gmail_user = '' # Gmail Username
+	gmail_pwd = '' #APP Password
 	gmail_config = (gmail_user, gmail_pwd)
 	return gmail_config
